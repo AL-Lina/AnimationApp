af5a7e505e8f8bd8584379c6f73f587735d002bc
AL-Lina <lina-sokolova00@mail.ru>
2022-01-13 00:04:31 +0300

    Create animation for SpringButton and SpringView

--- AnimationApp/ViewController.swift
+++ AnimationApp/ViewController.swift
@@ -5,15 +5,52 @@
 //  Created by Alina Sokolova on 12.01.22.
 //
 
-import UIKit
+import Spring
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var springView: SpringView!
+    enum Animation {
+        case fisrtAnimation
+        case secondAnimation
+        case thirdAnimation
     }
+    
+    private var currentAnimation = Animation.fisrtAnimation
+    
+    
 
-
+    @IBAction func runButtonAnimation(_ sender: SpringButton) {
+        switch currentAnimation {
+        case .fisrtAnimation:
+            sender.setTitle("Run", for: .normal)
+            springView.animation = "pop"
+            springView.curve = "easeInOut"
+            springView.force = 2
+            springView.duration = 1
+            springView.delay = 0.3
+            sender.animation = "squeeze"
+            sender.animate()
+        case .secondAnimation:
+            sender.setTitle("SecondAnimation", for: .normal)
+            springView.animation = "shake"
+            springView.curve = "easenOutCubic"
+            springView.force = 0.57
+            springView.duration = 1.80
+            springView.delay = 0.3
+            sender.animation = "shake"
+            sender.animate()
+        case .thirdAnimation:
+            sender.setTitle("ThirdAnimation", for: .normal)
+            springView.animation = "fall"
+            springView.curve = "easeInOutCubic"
+            springView.force = 0.57
+            springView.duration = 1.80
+            springView.delay = 0.3
+            sender.animation = "zoomIn"
+            sender.animate()
+        }
+    }
+    
 }
 
